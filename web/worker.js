20a9c7db12318a552c72f5c01dd3b8ba7d7d20f7
majda107 <majda.trpkos@gmail.com>
2020-05-26 20:36:05 +0200

    Fixed service worker

--- web/worker.js
+++ web/worker.js
@@ -3,27 +3,33 @@ CACHES = [
     'dist/css/style.min.css',
     'index.html',
     'projects/projects.html',
+    'resources/resources.html',
     'script.js',
+    'worker.js',
     'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap',
     'fonts/odachi/odachi.css',
     'fonts/odachi/odachi-webfont.woff',
     'fonts/odachi/odachi-webfont.woff2',
-    'worker.js',
+    
     'res/icons/facebook.svg',
     'res/icons/github.svg',
     'res/icons/github.png',
     'res/icons/discord.svg',
     'res/icons/telegram.svg',
+
     'res/setup/linux.png',
     'res/setup/ide.png',
     'res/setup/setup_light.jpg',
     'res/setup/keyboard.jpg',
+
     'res/majda.png',
+    'res/majda-dark.png',
+    'res/portrait.jpg',
+
+    'res/projects/font.png',
     'res/projects/vxgi.jpg',
     'res/projects/tracing.jpg',
-    'res/projects/font.png',
-    'res/projects/majdacraft.jpg',
-    'res/portrait.png'
+    'res/projects/majdacraft.jpg'
 ]
 
 self.addEventListener('fetch', function (event) {
